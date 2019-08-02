33a2382adcd9dbdb80b32913c771a51806180fd0
Supremenine <50431761+jmtlockridge@users.noreply.github.com>
2019-08-01 20:07:32 -0500

    Revert "edited weather.js and added currency.js"
    This reverts commit 81e650aef507a73fefd42997ea2c566fa76b19e8.

--- js/weather.js
+++ js/weather.js
@@ -7,10 +7,23 @@ function getWeatherInfo(capital) {
     });
 }
 
+ var date = new Date(sec * 1000); {
+   var timestr = date.toLocaleTimeString();
+   console.log(date, timestr);
+ }
 
-function degToCompass(num) {
-    var val = Math.floor((num / 22.5) + 0.5);
-    var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-    return arr[(val % 16)];
-
+ function description(d) {
+    var description = d.weather[0].description;
+    document.getElementById('description').innerHTML = description;
+     document.getElementById('temp').innerHTML = celcius + '&deg;';
+     document.getElementById('location').innerHTML = d.name;
+     
+     if( description.indexOf('rain') > 0 ) {
+       document.body.className = 'rainy';
+   } else if( description.indexOf('cloud') > 0 ) {
+       document.body.className = 'cloudy';
+   } else if( description.indexOf('sunny') > 0 ) {
+       document.body.className = 'sunny';
+   }
 }
+
