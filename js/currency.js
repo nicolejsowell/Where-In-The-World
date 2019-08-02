6d79b5fc35049330beb289a9138e20f133a290a9
Supremenine <50431761+jmtlockridge@users.noreply.github.com>
2019-08-01 20:33:44 -0500

    currency api

--- js/currency.js
+++ js/currency.js
@@ -1,23 +1,11 @@
 var apiKey = 'd7a838a989msh34298797289bd25p1df66djsn0b278833dc0e';
 function getExchangeRate(symbol) {
-    $.ajax({
+    return jQuery.ajax({
         type: "GET",
         beforeSend: function (request) {
             request.setRequestHeader("x-rapidapi-host", "currency-converter5.p.rapidapi.com");
             request.setRequestHeader("x-rapidapi-key", apiKey);
-        },
-        url: "https://currency-converter5.p.rapidapi.com/currency/convert",
-        data: {
-            "format": "json",
-            "from": "USD",
-            "to": symbol,
-            "amount": "1"
-        },
-        success: function (msg) {
-            console.log(msg.rates[symbol].rate);
         }
+
     });
 }
-getExchangeRate('JPY');
-getExchangeRate('EUR');
-getExchangeRate('AUD');
