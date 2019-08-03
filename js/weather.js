2b5478cae846ac6484135213f8471764c2a46335
Nicole Hendricks <nikkisowelltk@gmail.com>
2019-08-03 10:29:41 -0500

    updated html

--- js/weather.js
+++ js/weather.js
@@ -7,23 +7,3 @@ function getWeatherInfo(capital) {
     });
 }
 
- var date = new Date(sec * 1000); {
-   var timestr = date.toLocaleTimeString();
-   console.log(date, timestr);
- }
-
- function description(d) {
-    var description = d.weather[0].description;
-    document.getElementById('description').innerHTML = description;
-     document.getElementById('temp').innerHTML = celcius + '&deg;';
-     document.getElementById('location').innerHTML = d.name;
-     
-     if( description.indexOf('rain') > 0 ) {
-       document.body.className = 'rainy';
-   } else if( description.indexOf('cloud') > 0 ) {
-       document.body.className = 'cloudy';
-   } else if( description.indexOf('sunny') > 0 ) {
-       document.body.className = 'sunny';
-   }
-}
-
